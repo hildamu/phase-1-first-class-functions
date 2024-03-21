209934c069be451e35a5214eb4da8e6716ab12ad
hildamu <mukamihilda437@gmail.com>
2024-03-21 12:25:23 +0300

    include new files

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(Callback, name){
+    Callback(name)
+}
+receivesAFunction(name ,"spy");
+
+function returnsANamedFunction(funcType){
+    return  (funcType ) = funcType => console.log('i love food');
+    }
+
+    function returnsAnAnonymousFunction(anonymous){
+        return (anonymous)  => console.log('to deeply equal');
+        
+        }
+       
+
